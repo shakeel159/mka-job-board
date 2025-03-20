80bd57b0ea710c5c55cf051e6d129864dda8e7e9
shakeel159 <jazibshaq@gmail.com>
2025-03-20 17:43:21 -0500

    fixes to gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Detect if running in production (GitHub Pages)
-const isGitHubPages = process.env.NODE_ENV === 'production';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: isGitHubPages ? '/mka-job-board/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/your-repo-name/' : '/', 
 })
